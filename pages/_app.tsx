9607d67dbfeaf62410ac1c5d5f1e49fb6d07ece4
Pablo P Varela <pablo@pablopunk.com>
2021-07-07 17:28:14 +0200

    Match iOS title bar to header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState, useEffect } from 'react'
 import 'tailwindcss/tailwind.css'
 import 'components/styles.css'
 import { Page } from 'components/Layout'
@@ -6,19 +6,43 @@ import { NextSeo } from 'next-seo'
 import { SITE_DESCRIPTION, SITE_NAME } from 'config'
 import Head from 'next/head'
 import SimpleReactLightbox from 'simple-react-lightbox'
+import useTheme from 'hooks/useTheme'
 
-const App: FunctionComponent<any> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <link rel="shortcut icon" href="/favicon.png" />
-    </Head>
-    <Page>
-      <NextSeo title={SITE_NAME} description={SITE_DESCRIPTION} />
-      <SimpleReactLightbox>
-      <Component {...pageProps} />
-</SimpleReactLightbox>
-    </Page>
-  </>
-)
+const THEME = {
+  dark: {
+    bg: '#201d2b'
+  },
+  light: {
+    bg: '#f1f1f1'
+  }
+}
+
+const App: FunctionComponent<any> = ({ Component, pageProps }) => {
+  const [theme] = useTheme()
+  const [titleBarColor, setTitleBarColor] = useState(THEME.dark.bg)
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      setTitleBarColor(THEME.dark.bg)
+    } else {
+      setTitleBarColor(THEME.light.bg)
+    }
+  }, [theme])
+
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="theme-color" content={titleBarColor} />
+      </Head>
+      <Page>
+        <NextSeo title={SITE_NAME} description={SITE_DESCRIPTION} />
+        <SimpleReactLightbox>
+          <Component {...pageProps} />
+        </SimpleReactLightbox>
+      </Page>
+    </>
+  )
+}
 
 export default App
